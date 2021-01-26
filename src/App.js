279398c1a9c88382a886a7fc558c783fc9420f35
geigea84 <geigea84@gmail.com>
2021-01-25 22:31:26 -0600

    20.3.4 requested push

--- src/App.js
+++ src/App.js
@@ -5,20 +5,51 @@ component that houses all of the other components. To
 effect any change on the application, we need to either 
 modify this file or add components inside it. */
 
-import React from 'react';
+import React, {useState} from 'react';
 import Nav from './components/Nav'
 import About from './components/About';
+import Gallery from './components/Gallery';
 import './App.css';
 
 function App() {
-  return (
-    <div>
-        <Nav></Nav>
-        <main>
-            <About></About>
-        </main>
-    </div>
-  );
+    //20.3.4
+    const [categories] = useState([
+        {
+            name: "commercial",
+            description: "Photos of grocery stores, food trucks, and other commercial projects"
+        },
+        {
+            name: "portraits",
+            description: "Portraits of people in my life"
+        },
+        {
+            name: "food",
+            description: "Delicious delicacies"
+        },
+        {
+            name: "landscape",
+            description: "Fields, farmhouses, waterfalls, and the beauty of nature"
+        }
+    ]);
+
+    //20.3.4
+    const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
+    return (
+        <div>
+            <Nav
+                categories={categories}
+                setCurrentCategory={setCurrentCategory}
+                currentCategory={currentCategory}
+            ></Nav>
+            <main>
+                <div>
+                    <Gallery></Gallery>
+                    <About></About>
+                </div>
+            </main>
+        </div>
+    );
 }
 
 export default App;
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,35 +1,28 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {capitalizeFirstLetter} from '../../utils/helpers';
 
-function Nav() {
-    //20.1.6
-    const categories = [
-        {
-            name: "commercial",
-            description: "Photos of grocery stores, food trucks, and other commercial projects"
-        },
-        {
-            name: "portraits",
-            description: "Portraits of people in my life"
-        },
-        {
-            name: "food",
-            description: "Delicious delicacies"
-        },
-        {
-            name: "landscape",
-            description: "Fields, farmhouses, waterfalls, and the beauty of nature"
-        }
-    ];
+function Nav(props) {
+    //20.3.4
+    const {
+        categories = [],
+        setCurrentCategory,
+        currentCategory
+    } = props;
 
-    function categorySelected(name) {
-        console.log(`${name} clicked`);
-    }
+    //20.3.4 come back and recap
+    useEffect(() => {
+        document.title = capitalizeFirstLetter(currentCategory.name);
+    }, [currentCategory]);
 
+    //20.1.6 
     return (
-        <header>
+        <header className="flex-row px-1">
             <h2>
                 <a data-testid="link" href="/">
-                    <span role="img" aria-label="camera">📸</span> Oh Snap!
+                    <span role="img" aria-label="camera">
+                        {' '}📸
+                    </span>{' '}
+                    Oh Snap!
                 </a>
             </h2>
             <nav>
@@ -39,16 +32,18 @@ function Nav() {
                             About me
                         </a>
                     </li>
-                    <li>
+                    <li className="mx-2">
                         <span>Contact</span>
                     </li>
                     {categories.map((category) => (
                         <li 
-                            className="mx-1"
+                            className={`mx-1 ${
+                                currentCategory.name === category.name && 'navActive'
+                            }`} 
                             key={category.name}
                         >
-                            <span onClick={() => categorySelected(category.name)}>
-                                {category.name}
+                            <span onClick={() => {setCurrentCategory(category)}}>
+                                {capitalizeFirstLetter(category.name)}
                             </span>
                         </li>
                     ))}
